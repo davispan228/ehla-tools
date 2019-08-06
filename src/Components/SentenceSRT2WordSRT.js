b3476f64fcec4b398d942b6e3542e187492a901b
Davis Pan <davispan228@gmail.com>
2019-08-06 21:57:36 +0800

    add word_count in sentence csv

--- src/Components/SentenceSRT2WordSRT.js
+++ src/Components/SentenceSRT2WordSRT.js
@@ -50,6 +50,9 @@ export default class SentenceSRT2WordSRT extends Component {
     return outFilePathComponent.join(".");
   }
 
+
+  
+
   convert = async (file) => {
     let content = await this.readFile(file);
     let parsed = parseSRT(content);
@@ -62,10 +65,25 @@ export default class SentenceSRT2WordSRT extends Component {
     let fn = this.getFileName(file);
     saveAs(blob, `${fn}_word.srt`);
 
+
+    const specialWords = [
+      'e.g.', 'a.m.', 'p.m.'
+    ];
     //output sentence CSV
     outputData = "\uFEFF";
-    outputData += "start_ms,end_ms,id,en,hk,cn,tw,jp,kr\n";
-    outputData += parsed.map(_=>`${_.start},${_.end},,"${_.text}",,,,,`).join("\n");
+    outputData += "start_ms,end_ms,id,word_count,en,hk,cn,tw,jp,kr\n";
+    outputData += parsed.map(_=>{
+      let text = _.text;
+      specialWords.forEach(_=>text=text.replace(_, 'word'));
+      //replace
+      text = text.replace(/-/g,' ');
+      text = text.replace(/,/g,' ');
+      text = text.replace(/./g,' ');
+      text = text.replace(/!/g,' ');
+      text = text.replace(/;/g,' ');
+      const wordCount = text.split(' ').filter(_=>_!='').length;
+      return `${_.start},${_.end},,${wordCount},"${_.text}",,,,,`.join("\n");
+    });
     blob = new Blob([outputData], {type: "text/plain;charset=utf-8"});
     saveAs(blob, `${fn}.csv`);
     
